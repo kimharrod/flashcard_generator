a9958aaedaea1021e1f071e5c64353a1c10d986d
Kimberly Harrod <kim@fios.com>
2017-04-21 10:38:42 -0500

    Added readfile for indexing cards

--- cards.js
+++ cards.js
@@ -1,7 +1,26 @@
+'use strict';
+
+// npm package dependencies
 var inquirer = require("inquirer");
 var fs = require("fs");
 
+// variables to log a card index and keep track of current card numnber
+var cardNum = 1;
+var cardIndex = [];
+
+// use inquirer to prompt user to choose the application activity path
 function main () {
+
+// opens and reads number of lines in index file to determine next card number
+fs.readfile("card-index.txt", "utf8", function(error, data) {
+
+		// first check to see if a card index file already exists
+		if (!error) {
+			var indexLines = data.split("\n");
+			cardNum = indexLines.length;
+		} // end if card-index.txt file exists
+	}); // end readFile callback
+
 inquirer.prompt([
 	{
 		type: 'list',
@@ -46,8 +65,7 @@ inquirer.prompt([
 
 	} //end switch
 
-});
+  });
 
 } //end function main
 
-}
